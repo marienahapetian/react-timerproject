04b6d3fb9c6671e2918901ce1cde6cb632219a7d
Marie <marie.nahapetian@gmail.com>
2025-07-26 13:16:03 +0200

    fix ref bug

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,19 +3,16 @@ import { useState, useRef } from "react";
 export default function Player() {
   const inputPlayerName = useRef();
   const [playerName, setPlayerName] = useState('');
-  const [submitted, setSubmitted] = useState(false);
-
 
   function handleClick(){
     setPlayerName(inputPlayerName.current.value);
-    setSubmitted(true);
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted? playerName: 'unknown entity'}</h2>
+      <h2>Welcome {playerName?? 'unknown entity'}</h2>
       <p>
-        <input ref={inputPlayerName} type="text" onChange={handleChange} value={playerName}/>
+        <input ref={inputPlayerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
